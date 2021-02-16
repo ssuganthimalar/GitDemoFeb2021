74e5a246943851a94ac33fdecd011f965d7bf7c9
ssuganthimalar <ssuganthimalar@gmail.com>
2021-02-16 17:44:20 +0000

    Gittestfeb removed 1 test

--- src/test/java/GitTest01.java
+++ src/test/java/GitTest01.java
@@ -7,11 +7,6 @@ public class GitTest01 {
     }
     @Test
     public void test2(){
-        System.out.println("Running Git test2");
+        System.out.println("Running Git test1");
     }
-    @Test
-    public void test3(){
-        System.out.println("Running Git test3");
-    }
-
 }
