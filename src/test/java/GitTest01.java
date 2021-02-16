8569ec739c08174bc42d43b5f803675b16af52ef
ssuganthimalar <ssuganthimalar@gmail.com>
2021-02-16 15:16:53 +0000

    Gittestfeb added 1 test

--- src/test/java/GitTest01.java
+++ src/test/java/GitTest01.java
@@ -7,6 +7,11 @@ public class GitTest01 {
     }
     @Test
     public void test2(){
-        System.out.println("Running Git test1");
+        System.out.println("Running Git test2");
     }
+    @Test
+    public void test3(){
+        System.out.println("Running Git test3");
+    }
+
 }
